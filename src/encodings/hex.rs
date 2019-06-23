dd76dad54aa528032acf65f9c848f72a2f3f7071
Benjamin Strauch <b.strauch@fu-berlin.de>
2019-06-23 15:41:13 +0200

    Fix byte to hex encoding and rustfmt.

--- src/encodings/hex.rs
+++ src/encodings/hex.rs
@@ -8,12 +8,11 @@ pub trait HexPrintable {
 impl HexPrintable for Vec<u8> {
     fn as_hex(&self) -> String {
         self.iter()
-            .flat_map(|b| vec![b & 15, b >> 4])
+            .flat_map(|b| vec![b >> 4, b & 0b00001111])
             .map(|b| char::from_digit(b as u32, 16).unwrap())
             .collect::<String>()
     }
 }
-
 
 /// Decodes the bytes from a hex string.
 pub trait FromHex {
@@ -23,12 +22,14 @@ pub trait FromHex {
 impl FromHex for &str {
     fn from_hex(&self) -> Vec<u8> {
         // map the hex characters to numbers (e.g. 1 -> 1, A -> 10, F -> 15)
-        let hex_bytes = self.chars()
+        let hex_bytes = self
+            .chars()
             .map(|c| c.to_digit(16).unwrap() as u8)
             .collect::<Vec<u8>>();
 
         // each two hex characters represent one byte
-        hex_bytes.chunks(2)
+        hex_bytes
+            .chunks(2)
             .map(|chunk| (chunk[0] << 4) + chunk[1])
             .collect::<Vec<u8>>()
     }
